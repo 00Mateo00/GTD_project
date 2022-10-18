b7c9cef3377a8218bb91f3ac8cbfe928995c87db
Mateo Ossa <trevorsoul.ts@gmail.com>
2022-10-18 17:50:40 -0300

    a lot of changes

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ContextWrapper from './context/ContextWrapper';
+import { BrowserRouter } from 'react-router-dom';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ContextWrapper>
+        <App/>
+      </ContextWrapper>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
