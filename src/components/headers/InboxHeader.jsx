2dab03cab9fce58699f816cef5e9954a61237303
Mateo Ossa <trevorsoul.ts@gmail.com>
2022-12-05 02:40:44 -0300

    test2

--- src/components/headers/InboxHeader.jsx
+++ src/components/headers/InboxHeader.jsx
@@ -6,52 +6,10 @@ import { useContext } from 'react'
 import GlobalContext from '../../context/GlobalContext'
 
 export const InboxHeader = () => {
-  const [dayEvents, setDayEvents] = useState([]);
-  const {
-    setOnShowModal,
-    filteredTicklerFileEvents,
-    setSelectedTicklerFileEvent,
-    dispatchCallTicklerFileEvent,
-    handleChecked
-  } = useContext(GlobalContext);
-
-  useEffect(() => {
-    const events = filteredTicklerFileEvents.filter(
-      (e) => dayjs(e.day).format("DD-MM-YY") === dayjs().format("DD-MM-YY")
-    );
-    setDayEvents(events);
-  }, [filteredTicklerFileEvents]);
-
-
-  const events = dayEvents.sort((a,b)=> a.id - b.id).map(
-    (e, i) =>
-      i < 3 && (
-        <div
-          key={i}
-          onClick={() => {
-            setSelectedTicklerFileEvent(e);
-            setOnShowModal("/Tickler-File");
-          }}
-          className={`${e.label} tickler-day-event`}
-        >
-          <span>{e.title}</span>
-          <button
-            onClick={(event) => {
-              event.stopPropagation();
-              handleChecked(e,dispatchCallTicklerFileEvent);
-            }}
-            className="tickler-event__check"
-          >
-            <span className="material-symbols-outlined">
-              {e.checked === 0? "check_box_outline_blank" : "select_check_box"}
-            </span>
-          </button>
-        </div>
-      )
-  );
+  
   
 
   return (
-    <div>{events}</div>
+    <div>dsadsa</div>
   )
 }
--- src/components/inbox/Inbox.jsx
+++ src/components/inbox/Inbox.jsx
@@ -1,11 +1,60 @@
-
+import dayjs from "dayjs";
+import React from "react";
+import { useContext } from "react";
+import GlobalContext from "../../context/GlobalContext";
+import { MenuModal } from "../modals/MenuModal";
+import "./inbox.scss";
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 export const Inbox = () => {
+  const [dayEvents, setDayEvents] = useState([]);
   const {
+    setOnShowModal,
+    filteredTicklerFileEvents,
+    setSelectedTicklerFileEvent,
+    dispatchCallTicklerFileEvent,
+    handleChecked,
     savedActionableTODOS,
     savedCalendarEvents,
     savedTicklerFileEvents
   } = useContext(GlobalContext)
+
+
+  useEffect(() => {
+    const events = filteredTicklerFileEvents.filter(
+      (e) => dayjs(e.day).format("DD-MM-YY") === dayjs().format("DD-MM-YY")
+    );
+    setDayEvents(events);
+  }, [filteredTicklerFileEvents]);
+
+
+  const events = dayEvents.sort((a,b)=> a.id - b.id).map(
+    (e, i) =>
+      i < 3 && (
+        <div
+          key={i}
+          onClick={() => {
+            setSelectedTicklerFileEvent(e);
+            setOnShowModal("/Tickler-File");
+          }}
+          className={`${e.label} tickler-day-event`}
+        >
+          <span>{e.title}</span>
+          <button
+            onClick={(event) => {
+              event.stopPropagation();
+              handleChecked(e,dispatchCallTicklerFileEvent);
+            }}
+            className="tickler-event__check"
+          >
+            <span className="material-symbols-outlined">
+              {e.checked === 0? "check_box_outline_blank" : "select_check_box"}
+            </span>
+          </button>
+        </div>
+      )
+  );
   return (
     <div className="inbox-wrapper">
       <button onClick={()=>console.log(dayjs().valueOf())}>test</button>
